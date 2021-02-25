8f7ac80784acaf6133db264223219208443d3b34
Jesper Persson <jepe6324@student.uu.se>
2021-02-25 14:30:46 +0100

    Player movement and collision, and a random test box for collision

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    CircleCollider2D collisionRadius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collisionRadius = GetComponent<CircleCollider2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision!");
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    public float maxX, minX, maxY, minY;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+        ClampPosition();
+    }
+
+    void HandleMovement()
+	{
+        Vector3 position = gameObject.transform.position;
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        position.x += x * speed * Time.deltaTime;
+        position.y += y * speed * Time.deltaTime;
+
+        gameObject.transform.position = position;
+    }
+    void ClampPosition()
+	{
+        Vector3 position = gameObject.transform.position; // having to do this twice feels kinda wasteful
+
+        position.x = Mathf.Clamp(position.x, minX, maxX);
+        position.y = Mathf.Clamp(position.y, minY, maxY);
+
+        gameObject.transform.position = position;
+    }
+}
